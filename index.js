311bfe511faba259fe1be2db2f62bdb925205b3d
Nima Boscarino <nboscarino@hatfieldgroup.com>
2017-01-09 17:17:19 -0800

    Add license, and setup example page

---
+++ index.js
@@ -0,0 +1,34 @@
+(function (factory, window) {
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], factory);
+
+        // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('leaflet'));
+    }
+
+    // attach your plugin to the global 'L' variable
+    if (typeof window !== 'undefined' && window.L) {
+        window.L.PanelManager = factory(L);
+    }
+}(function (L) {
+    var PanelManager = {
+        options: {
+        },
+
+        version: '0.1.0',
+
+        getVersion: function() {
+            var self = this;
+            console.log(self.version);
+        },
+
+        addTo: function(map) {
+            map.PanelManager = PanelManager;
+        }
+    };
+    // return your plugin when you are done
+    return PanelManager;
+}, window));
+
---
+++ mapConfig.js
@@ -0,0 +1,12 @@
+﻿var bounds = new L.LatLngBounds(new L.LatLng(-85, -200), new L.LatLng(85, 250));
+var map = L.map('map', {
+    attributionControl: false,
+    maxBounds: bounds,
+    maxBoundsViscosity: 0.4,
+}).setView([49.2827, -123.1207], 13);
+// disable double click to zoom
+map.doubleClickZoom.disable();
+map.options.minZoom = 3;
+// NOTE: do not use stamen without API key for production
+L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', { // replace path with link to tile server...
+}).addTo(map);
